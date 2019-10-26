30806ec665d71e5ccd27d7aa7866c97094fee451
User <motsamailegakwa@gmail.com>
2019-10-26 15:54:44 +0200

    customer model done

--- fleet_management/core/models.py
+++ fleet_management/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+type_options = [
+    ('Student', 'Student'),
+    ('Staff', 'Staff'),
+]
+
 # Create your models here.
+class Customer(models.Model):
+    user_ID = models.IntegerField()
+    user_first_name = models.CharField(max_length =100) #edit the class
+    user_last_name = models.CharField(max_length=100)
+    user_email = models.CharField(max_length =200)
+    user_DOB = models.DateField()
+    user_type = models.CharField(max_length =100, choices = type_options)
