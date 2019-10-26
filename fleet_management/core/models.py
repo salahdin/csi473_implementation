8080b9ff6d21da214293cc503a10939bbf17081f
Zibo Matiwaza <zibomatiwaza98@gmail.com>
2019-10-26 16:04:34 +0200

    model made

--- fleet_management/core/models.py
+++ fleet_management/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Vehicle(models.Model):
+    vehicle_id = models.IntegerField()
+    vehicle_type = models.CharField(max_length=100)
+    vehicle_description = models.TextField(max_length=255)
+    vehicle_Pnumber = models.CharField(max_length=7)
