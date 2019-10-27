ad7768a11ecf5103555e9c74d27b76accfa929a0
Zibo Matiwaza <zibomatiwaza98@gmail.com>
2019-10-27 16:04:45 +0200

    logout and login added

--- fleet_management/accounts/views.py
+++ fleet_management/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 # Create your views here.
 def signup_view(request):
@@ -7,9 +8,27 @@ def signup_view(request):
     if request.method =='POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
             #remember to log the user in
+            login(request, user)
             return redirect('accounts:signup')
         else:
             form = UserCreationForm
     return render(request, 'accounts/signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            #log in the user
+            user = form.get_user()
+            login(request, user)
+            return redirect('accounts:signup')
+    else:
+        form = AuthenticationForm()
+    return render(request, "accounts/login.html", {'form':form })
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('accounts:signup')
