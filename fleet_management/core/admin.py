48a7cae6151738185f7ff832c6cb3d8722ffd3ec
uliyan <uzziface@gmail.com>
2019-11-04 12:46:28 +0200

    Update admin.py
    edit admin

--- fleet_management/core/admin.py
+++ fleet_management/core/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
-admin.site.register(Vehicle)
-admin.site.register(Reservation)
-admin.site.register(ReturnVehicle)
+
+class ReservationInline(admin.TabularInline):
+    model = Reservation
+
+
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'rent_from', 'rent_to')
+    search_fields = ['vehicle']
+
+
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ('vehicle_name','vehicle_type','photo')
+    list_filter = ['vehicle_type']
+
+
+admin.site.register(Reservation, ReservationAdmin)
+admin.site.register(Vehicle, VehicleAdmin)
